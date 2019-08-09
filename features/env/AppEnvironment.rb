8906d3266e36a294f5176a642d2f0b5f9d0353e7
Daniel Santos <daniel.santos@koin.local>
2019-08-09 12:21:53 -0300

    Fixed environments

--- features/env/AppEnvironment.rb
+++ features/env/AppEnvironment.rb
@@ -36,13 +36,19 @@ class AppEnvironment
       ENV['PASSWORD_BUYER'] = 'koin@123'
       ENV['EMAIL_KOIN'] = 'naoresponder@koin.com.br'
 
-      ENV['CONDUCTOR_ENV'] = 'http://pre-prd-conductor.koin.com.br/pre-prd/Controle?manipulador=Login'
+      ENV['CONDUCTOR_ENV'] = 'http://hmg-risks-conductor.koin.com.br/hmg-risks/Controle?manipulador=Login'
       ENV['CONDUCTOR_USER'] = 'daniel.santos'
       ENV['CONDUCTOR_PASSWORD'] = 'Koin@123'
 
       ENV['ACQUARIUS_ENV'] = 'http://hmg-risks-aquarius.koin.com.br/#/login'
       ENV['AQUARIUS_USER'] = 'daniel.santos'
       ENV['AQUARIUS_PASSWORD'] = 'koin@123'
+      ENV['AQUARIUS_NAME'] = 'Daniel Santos '
+
+      ENV['CHECKOUT_URL'] = 'http://hmg-risks-institucional.koin.com.br/payment/complete/#'
+
+      ENV['CPF_APPRROVED'] = '354.574.310-10'
+      ENV['CPF_DISAPPRROVED'] = '451.625.980-03'
 
       ENV['AUTHORIZATION_INCLUDE_API'] = '8F6EC0BCB5634511A01B67785054E19E,VbH1VC/EjkeSuehEjetcjjdAMRCC/vBtDL8Z980isx2PUkLr2EHOrpkNRhydjsFy90JZZb59t5mcK2CNBYNhnA==,1560797432477'
       ENV['AUTHORIZATION_REQUEST_API'] = '8F6EC0BCB5634511A01B67785054E19E,46/MzUZnkJEf8QFRgJpwa+T5vd35XJ1Sp6HdbWi/O94RI/qAtyBwNQGwNwBQeQJVohUFQeBT3CPdHyqw1QPUFA==,1563296976702'
